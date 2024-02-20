151103cad5522706dea41a186f6733bfc98a41f4
bagusyatma <42886214+bagusyatma@users.noreply.github.com>
2024-02-20 15:44:05 +0700

    export notes

---
+++ src/validator/exports/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const { ExportNotesPayloadSchema } = require("./schema");
+
+const ExportsValidator = {
+  validateExportNotesPayload: (payload) => {
+    const validationResult = ExportNotesPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = ExportsValidator;
